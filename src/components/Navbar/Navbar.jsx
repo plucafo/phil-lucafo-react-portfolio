632be4d20202f5b62fd7ffdaa6b52148ddb39142
plucafo <plucaf87@gmail.com>
2024-05-09 08:38:51 -0700

    added active link indicator

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("#");
+
+  const handleLinkClick = (event, link) => {
+    event.preventDefault;
+    setActiveLink(link);
+    console.log("Active link set to:", link);
+    setMenuOpen(false);
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -22,18 +29,44 @@ export const Navbar = () => {
           alt="menu-button"
           onClick={() => setMenuOpen(!menuOpen)}
         />
-        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
+        <ul
+          className={`${styles.menuItems} ${menuOpen ? styles.menuOpen : ""}`}
+        >
           <li>
-            <a href="#about">About</a>
+            <a
+              href="#about"
+              className={activeLink === "#about" ? styles.active : ""}
+              onClick={(e) => handleLinkClick(e, "#about")}
+            >
+              About
+            </a>
           </li>
           <li>
-            <a href="#portfolio">Portfolio</a>
+            <a
+              href="#portfolio"
+              className={activeLink === "#portfolio" ? styles.active : ""}
+              onClick={(e) => handleLinkClick(e, "#portfolio")}
+            >
+              Portfolio
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a
+              href="#contact"
+              className={activeLink === "#contact" ? styles.active : ""}
+              onClick={(e) => handleLinkClick(e, "#contact")}
+            >
+              Contact
+            </a>
           </li>
           <li>
-            <a href="#resume">Resume</a>
+            <a
+              href="#resume"
+              className={activeLink === "#resume" ? styles.active : ""}
+              onClick={(e) => handleLinkClick(e, "#resume")}
+            >
+              Resume
+            </a>
           </li>
         </ul>
       </div>
