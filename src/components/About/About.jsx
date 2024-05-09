319f23a7dfe38875e7ce9343d2323a649138e497
plucafo <plucaf87@gmail.com>
2024-05-09 09:01:35 -0700

    added css module for About component and some basic styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,8 @@ import { getImageUrl } from "../../utils";
 export const About = () => {
 
     return (
-        <div>testing...</div>
+        <div className={styles.about}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus possimus tempora tempore voluptatibus quod suscipit rem, harum dignissimos perspiciatis fugiat consequatur vitae deleniti adipisci explicabo asperiores modi cumque ab itaque iure blanditiis placeat officia corrupti architecto! Vitae, quod placeat? Iste nulla sint dolorem optio praesentium, vel aliquid sunt, fuga dolore, ab repudiandae minus nostrum. Molestiae rerum officiis consequatur, voluptatem pariatur quae maiores ad incidunt ab quasi eius deserunt quidem esse ipsam, sunt eaque illo, ullam cum rem iure! Placeat ipsa repellat delectus aliquam, cumque aliquid alias, quae esse ut obcaecati vel reiciendis sint laboriosam atque impedit aspernatur totam, hic exercitationem.</p>
+        </div>
     );
 }
