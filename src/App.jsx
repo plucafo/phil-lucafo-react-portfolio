e0fa13f55de3f33c5f2e1595a28d8d8e1218c252
plucafo <plucaf87@gmail.com>
2024-05-09 08:58:23 -0700

    adjusted header styling and created About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
+import { About } from "./components/About/About";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+
+    return (
+        <div>testing...</div>
+    );
+}
