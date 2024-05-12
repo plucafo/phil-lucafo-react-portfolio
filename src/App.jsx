57abc6f5e1948c28e8e685732aee2070e3abbf1c
plucafo <plucaf87@gmail.com>
2024-05-12 09:43:10 -0700

    added hero image to about page

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
-      <div className={styles.container}>
-          <Navbar />
-          <div className={styles.content}>
-              <About />
-              {/* Additional components can be added here */}
-          </div>
-          <Footer />
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.content}>
+        <About />
       </div>
+      <Footer />
+    </div>
   );
 }
 
